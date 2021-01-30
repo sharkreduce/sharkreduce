aaa86545696bf4899ea27a6fd0360b4f4df9746f
Joseph Birkner <j.birkner@klebert-engineering.de>
2021-01-30 12:27:43 +0100

    Small readability improvements and bugfix for reducer.

--- scripts/reducer.py
+++ scripts/reducer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-#tshark -t e -Ndmnt -r test.pcap -T fields -e frame.time -e eth.src_resolved -e eth.dst_resolved  -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e tcp.len -e udp.length -e tcp.srcport -e tcp.dstport -e udp.srcport -e udp.dstport -e _ws.col.Time '(eth.type == 0x800) or (eth.type == 0x86dd)' > all.tsv
+# tshark -t e -Ndmnt -r test.pcap -T fields -e frame.time -e eth.src_resolved -e eth.dst_resolved  -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e tcp.len -e udp.length -e tcp.srcport -e tcp.dstport -e udp.srcport -e udp.dstport -e _ws.col.Time '(eth.type == 0x800) or (eth.type == 0x86dd)' > all.tsv
 
 import argparse
+from collections import defaultdict
 
-DISPLAY_FILTER='!(arp or llc or homeplug-av or (eth.type == 0x8912) or (eth.type == 0x8912))' # 
+# Are these microseconds or milliseconds?
+MAX_BIN_AGE = 1_000_000
+
 
 class Packet:
     def __init__(self, line):
@@ -14,6 +17,7 @@ class Packet:
         self.ethdst = fields[2]
         self.src = fields[3]
         self.dst = fields[4]
+        # TODO: Determine protocol by looking at destination port.
         self.proto = fields[5]
         self.tcplen = fields[6]
         self.udplen = fields[7]
@@ -35,7 +39,7 @@ class Packet:
         self.time = int(self.time2.split('.')[0] + self.time2.split('.')[1])
 
     def info(self):
-        return (self.src, self.dst, self.transport, self.srcport, self.dstport)
+        return self.src, self.dst, self.transport, self.srcport, self.dstport
 
 
 class Bin:
@@ -47,40 +51,40 @@ class Bin:
         self.protos = set()
 
     def info(self):
-        return (self.time1, str(self.count), str(self.size), str(self.protos))
+        return self.time1, str(self.count), str(self.size), str(self.protos)
     
-    def update(self, p):
+    def update(self, packet):
         ret = None
         if self.time == 0:
-            self.time = p.time
-            self.time1 = p.time1
-        elif p.time - self.time < threshold:
+            # Initial packet
+            self.time = packet.time
+            self.time1 = packet.time1
+        # JB: If flipped the operator here from < to >.
+        elif packet.time - self.time > MAX_BIN_AGE:
+            # New packet which is so far in the future that
+            # the bin is finished and resets.
             ret = self.info()
-            self.time = p.time
-            self.time1 = p.time1
+            self.time = packet.time
+            self.time1 = packet.time1
             self.count = 0
             self.size = 0
             self.protos = set()
         self.count += 1
-        self.size += p.size
-        self.protos.add(p.proto)
+        self.size += packet.size
+        self.protos.add(packet.proto)
         return ret
+
 
 class Stat:
     def __init__(self):
-        self.bins = {}
+        self.bins = defaultdict(Bin)
 
-    def update(self, p):        
-        k = p.info()
-        
-        bin = self.bins.get(k)
-        if not bin:
-            self.bins.update({k: Bin()})
-        x = self.bins[k].update(p)
-        if x:
-            return k + x
+    def update(self, packet):
+        packet_fivetuple = packet.info()
+        finished_bin_info = self.bins[packet_fivetuple].update(packet)
+        if finished_bin_info:
+            return packet_fivetuple + finished_bin_info
 
-threshold = 1000000
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -91,11 +95,11 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--routers", help="comma separated list of mac addresses of routers which should be ignored for per device analysis")
     args = parser.parse_args()
 
-    in_file = open(args.source, 'r') # , newline=''
+    in_file = open(args.source, 'r')  # , newline=''
     out_file = open(args.destination, 'a')
 
     if args.threshold:
-        threshold = args.threshold
+        MAX_BIN_AGE = args.threshold
 
     ignored = set()
     if args.ignored:
@@ -111,44 +115,42 @@ if __name__ == "__main__":
         if (not (mac in ignored)) and (not (mac in routers)):
             macs.add(mac)
 
-    def get_device(p):
+    def get_device(of_packet):
         macs = set()
 
-        put(macs, p.ethsrc)
-        put(macs, p.ethdst)
+        put(macs, of_packet.ethsrc)
+        put(macs, of_packet.ethdst)
 
         if (not macs) or (len(macs) > 1):
             print("local traffic")
             return None
         return macs.pop()
 
-    stats = {}
+    stats = defaultdict(Stat)
 
-    def update_stats(p):
-        device = get_device(p)
-        if not device:
+    def update_stats(of_packet):
+        device_for_packet = get_device(of_packet)
+        if not device_for_packet:
             return
-        
-        stat = stats.get(device)
-        if not stat:
-            stats.update({device: Stat()})
-        return stats[device].update(p)
+        return stats[device_for_packet].update(of_packet)
     
     last = 0
 
     line = in_file.readline()
     while line:
         p = Packet(line)
-        x = update_stats(p)
-        if x:
-            out_file.write('\t'.join((device,) + x) + '\n')
+        finished_bin = update_stats(p)
+        if finished_bin:
+            packet_device = get_device(p)
+            out_file.write('\t'.join((packet_device,) + finished_bin) + '\n')
         else:
-            if p.time - threshold > last:
+            # Print and remove bins that have become very old.
+            if p.time - MAX_BIN_AGE > last:
                 for device, stat in stats.items():
                     removals = set()
-                    for fivetuple, bin in stat.bins.items():
-                        if bin.time - threshold > last:
-                            out_file.write('\t'.join((device,) + fivetuple + bin.info()) + '\n')
+                    for fivetuple, packet_bin in stat.bins.items():
+                        if packet_bin.time - MAX_BIN_AGE > last:
+                            out_file.write('\t'.join((device,) + fivetuple + packet_bin.info()) + '\n')
                             removals.add(fivetuple)
                     for r in removals:
                         stat.bins.pop(r)
@@ -157,8 +159,8 @@ if __name__ == "__main__":
 
     for device, stat in stats.items():
         print(device)
-        for fivetuple, bin in stat.bins.items():
-                out_file.write('\t'.join((device,) + fivetuple + bin.info()) + '\n')
+        for fivetuple, packet_bin in stat.bins.items():
+            out_file.write('\t'.join((device,) + fivetuple + packet_bin.info()) + '\n')
 
     in_file.close()
     out_file.close()
